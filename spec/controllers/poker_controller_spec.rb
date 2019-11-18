57518b3bf044fe37c929716e07dea7b734a18383
i.kobayashi <ikobayashi@netprotections.co.jp>
2019-11-18 16:01:17 +0900

    重複の書き方を一行にしました。 ３つあった　it'カードが５個じゃないとき' をもっと具体的にしました。 controllerのテストも書いたのですが、うまくいきません。（poker_controller_spec.rbの11~19行目のメモにしてあるところ）レビューお願いします。

--- spec/controllers/poker_controller_spec.rb
+++ spec/controllers/poker_controller_spec.rb
@@ -8,14 +8,14 @@ RSpec.describe PokerController, type: :controller do
       expect(response.status).to eq(200)
     end
   end
-  describe '#check' do
-    #@cards,@result,@error_messageがきちんとしていればビューには表示され、@result,@error_messageはサービスのテストで立証済み @cardsを正しく拾ってこれてるかをチェックすればOK！
-    it 'ビューに対してきちんと返せているか' do
-      post :check
-      c = PokerCheckService::PokerCheckService.new("S1 S2 S3 S4 S5")
-      expect(assigns(:cards)).to eq c
-    end
-
-  end
+  # describe '#check' do
+  #     #@cards,@result,@error_messageがきちんとしていればビューには表示され、@result,@error_messageはサービスのテストで立証済み @cardsを正しく拾ってこれてるかをチェックすればOK！
+  #     it 'ビューに対してきちんと返せているか' do
+  #       post :check
+  #       c = PokerCheckService::PokerCheckService.new("S1 S2 S3 S4 S5")
+  #       expect(assigns(:cards)).to eq c
+  #     end
+  #
+  # end
 
 end
