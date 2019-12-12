0df1e39151b3ec5aaacb6ba379291aab030d55e1
i.kobayashi <ikobayashi@netprotections.co.jp>
2019-12-12 14:35:55 +0900

    urlを変更し、10行以上のロジックをプライベートメソッドに移しました。

--- app/services/poker_check_service.rb
+++ app/services/poker_check_service.rb
@@ -20,30 +20,10 @@ module PokerCheckService
        elsif @card.count == 5
           suits = [ @card[0][0] , @card[1][0] , @card[2][0] , @card[3][0] , @card[4][0] ]
           numbers = [ @card[0][1,2].to_i , @card[1][1,2].to_i , @card[2][1,2].to_i , @card[3][1,2].to_i , @card[4][1,2].to_i ]
-          if suits.grep(/[^SDHC]/).present?
-            @temp_message_s = ""
-            suits.each_with_index do |s,inx_s|
-             if s != "H" && s != "D" && s != "C" && s != "S"
-               @temp_message_s += "#{inx_s + 1}番目のカードのスートが不正です。(#{@card[inx_s]})"
-               @error_message_s = @temp_message_s + "半角英字大文字のスート（S,H,D,C）と数字（1〜13）の組み合わせでカードを指定してください。"
-             end
-           end
-          end
-          if numbers.select{ |n| n != 1 && n != 2 && n != 3 && n != 4 && n != 5 && n != 6 && n != 7 && n != 8 && n != 9 && n != 10 && n != 11 && n != 12 && n != 13 }.present?
-            @temp_message_n = ""
-            numbers.each_with_index do |n,inx_n|
-              if n != 1 && n != 2 && n != 3 && n != 4 && n != 5 && n != 6 && n != 7 && n != 8 && n != 9 && n != 10 && n != 11 && n != 12 && n != 13
-                @temp_message_n += "#{inx_n + 1}番目のカードの数字が不正です。(#{@card[inx_n]})"
-                @error_message_n = @temp_message_n + "半角英字大文字のスート（S,H,D,C）と数字（1〜13）の組み合わせでカードを指定してください。"
-             end
-            end
-          end
-          if @temp_message_s.present? && @temp_message_n.present?
-            @error_message = @temp_message_s + @temp_message_n + "半角英字大文字のスート（S,H,D,C）と数字（1〜13）の組み合わせでカードを指定してください。"
-          elsif @temp_message_s.present? && @temp_message_n.blank?
-            @error_message = @error_message_s
-          else @temp_message_s.blank? && @temp_message_n.present?
-            @error_message = @error_message_n
+          #ここの処理は10行異常なので、プライベートメソッドに任せる
+          if suits.grep(/[^SDHC]/).present? or
+              numbers.select{ |n| n != 1 && n != 2 && n != 3 && n != 4 && n != 5 && n != 6 && n != 7 && n != 8 && n != 9 && n != 10 && n != 11 && n != 12 && n != 13 }.present?
+            private_valid?
           end
         #カードの重複
           if @card.uniq.count != @card.count
@@ -142,6 +122,36 @@ module PokerCheckService
         @strong_number = 9
       end
 
+    end
+
+    private
+    def private_valid?
+      suits = [ @card[0][0] , @card[1][0] , @card[2][0] , @card[3][0] , @card[4][0] ]
+      numbers = [ @card[0][1,2].to_i , @card[1][1,2].to_i , @card[2][1,2].to_i , @card[3][1,2].to_i , @card[4][1,2].to_i ]
+      @temp_message_s = ""
+      suits.each_with_index do |s,inx_s|
+        if s != "H" && s != "D" && s != "C" && s != "S"
+          @temp_message_s += "#{inx_s + 1}番目のカードのスートが不正です。(#{@card[inx_s]})"
+          @error_message_s = @temp_message_s + "半角英字大文字のスート（S,H,D,C）と数字（1〜13）の組み合わせでカードを指定してください。"
+        end
+      end
+       if numbers.select{ |n| n != 1 && n != 2 && n != 3 && n != 4 && n != 5 && n != 6 && n != 7 && n != 8 && n != 9 && n != 10 && n != 11 && n != 12 && n != 13 }.present?
+         @temp_message_n = ""
+         numbers.each_with_index do |n,inx_n|
+           if n != 1 && n != 2 && n != 3 && n != 4 && n != 5 && n != 6 && n != 7 && n != 8 && n != 9 && n != 10 && n != 11 && n != 12 && n != 13
+             @temp_message_n += "#{inx_n + 1}番目のカードの数字が不正です。(#{@card[inx_n]})"
+             @error_message_n = @temp_message_n + "半角英字大文字のスート（S,H,D,C）と数字（1〜13）の組み合わせでカードを指定してください。"
+           end
+         end
+       end
+       if @temp_message_s.present? && @temp_message_n.present?
+         @error_message = @temp_message_s + @temp_message_n + "半角英字大文字のスート（S,H,D,C）と数字（1〜13）の組み合わせでカードを指定してください。"
+       elsif @temp_message_s.present? && @temp_message_n.blank?
+         @error_message = @error_message_s
+       else @temp_message_s.blank? && @temp_message_n.present?
+       @error_message = @error_message_n
+       end
+      @error_message
     end
   end
 end
